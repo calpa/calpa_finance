5fa1173d7ce2ed548e1ae834ed7c1601ea02411f
Calpa <calpaliu@gmail.com>
2022-08-02 13:09:10 -0700

    feat: use serverless cloud

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-(async () => {
-    const neo4j = require("neo4j-driver");
-    require("dotenv").config();
-    const fs = require('fs')
-    const path = require('path');
+const { schedule, api } = require('@serverless/cloud')
+
+const neo4j = require("neo4j-driver");
+require("dotenv").config();
+const fs = require('fs')
+const path = require('path');
+
+const {
+    NEO4J_URI: uri,
+    NEO4J_USERNAME: username,
+    NEO4J_PASSWORD: password,
+    NEO4J_DATABASE: database
+} = process.env
+
+let driver, session;
+
+async function connectPeople (person1Name, person2Name) {
     const query = fs.readFileSync(path.join(__dirname, 'people.cql'), 'utf8')
-
-    const {
-        NEO4J_URI: uri,
-        NEO4J_USERNAME: username,
-        NEO4J_PASSWORD: password,
-        NEO4J_DATABASE: database
-    } = process.env
-
-    const driver = neo4j.driver(uri, neo4j.auth.basic(username, password));
-    const session = driver.session({ database });
-
-    const person1Name = "Alice";
-    const person2Name = "David";
 
     try {
         // Write transactions allow the driver to handle retries and transient errors
@@ -24,11 +23,23 @@
             tx.run(query, { person1Name, person2Name })
         );
 
-        console.log(writeResult)
+        console.log(writeResult.records)
     } catch (error) {
         console.error("Something went wrong: ", error);
-    } finally {
-        await session.close();
     }
-    await driver.close();
-})();
+}
+
+// api.get('/', async (req, res) => {
+schedule.every('1 minute', async () => {
+    driver = neo4j.driver(uri, neo4j.auth.basic(username, password));
+    session = driver.session({ database });
+
+    await connectPeople("Alice", "Peter")
+    await connectPeople("Alice", "Bob")
+    await connectPeople("Bob", "Peter")
+    await connectPeople("Calpa", "Peter")
+
+    await session.close()
+    await driver.close()
+    // res.send('OK')
+})
